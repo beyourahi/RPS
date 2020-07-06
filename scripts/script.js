9c3eff258c90c46f7c6d3251840f30f516628f56
rahikhan <rahikhan360@gmail.com>
2020-07-06 15:42:02 +0600

    done with styling

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,12 @@
 // !Variable Declarations
 
 // !Main Code
+function RPS(yourChoice) {
+    let humanChoice, botChoice, result, message;
+
+    //humanChoice = yourChoice.id;
+    //botChoice =
+    //result = decideWinner(humanChoice, botChoice);
+    //message = finalmessage(result);
+    //rpsFrontEnd(yourChoice.id, botChoice.id, message);
+}
